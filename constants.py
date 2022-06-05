7224afb0f5ed8aefc0179f32abcd7d7259e707e5
ashishsth7586 <ashishsth7586@gmail.com>
2022-06-05 12:53:22 -0500

    added mainnet part

--- constants.py
+++ constants.py
@@ -8,13 +8,13 @@ TOKEN_MINT_ADDRESS = ""
 
 INIT_STREAM_INSTRUCTION = 3
 WITHDRAW_TOKEN_STRING = "withdraw_token"
-RPC_CLUSTER_URL = "https://api.devnet.solana.com"
+RPC_CLUSTER_URL = "https://api.mainnet-beta.solana.com"     # make sure you use third-party RPC Endpoint (like Syndica) since this endpoint is not stable.
 ZEBEC_PROGRAM_ID = "AknC341xog56SrnoK6j3mUvaD1Y7tYayx1sxUGpeYWdX"
 
 SYSTEM_PROGRAM_ID = "11111111111111111111111111111111"
 TOKEN_PROGRAM_ID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
 
-API_ZEBEC_URL = "http://127.0.0.1:5000"
+API_ZEBEC_URL = "https://api.zebec.io"
 FROM_WALLET_KEYPAIR = Keypair.from_secret_key(
     base58.b58decode(_SENDER_WALLET_ADDRESS_PRIVATE_KEY)
 )
